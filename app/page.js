6626279544318442e6c3c2cac57be938a7b13a50
bash <4gutzees@gmail.com>
2023-06-07 22:07:37 -0400

    update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,24 @@
-"use client";
-import Chatbot from './chat';
+import { useState } from 'react';
+
 export default function Home() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+
+  const sendMessage = () => {
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
+  const startNewChat = () => {
+    setMessages([]);
+  }
+
   return (
-    <main>
-      <Chatbot />
-    </main>
-  );
+    <div>
+      {/* chat messages will go here */}
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={sendMessage}>Send</button>
+      <button onClick={startNewChat}>New Chat</button>
+    </div>
+  )
 }
