c9cacef386e1c5115c35d3be76dfe88649cb6941
HMISTER <107431892+HMISTER03@users.noreply.github.com>
2023-06-14 14:05:42 -0400

    Merge
    Merge

--- app/api/openai.js
+++ app/api/openai.js
@@ -3,23 +3,23 @@ const { Configuration, OpenAIApi } = require("openai");
 const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 
 const configuration = new Configuration({
-    apiKey: apiKey,
+  apiKey: apiKey,
 });
 
 const openai = new OpenAIApi(configuration);
 
 export default async (req, res) => {
-    
-    console.log('__________________________________-')
-    console.log(req.body.query)
-    console.log('__________________________________-')
 
-    const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messageList,
-    });
+  console.log('__________________________________-')
+  console.log(req.body.query)
+  console.log('__________________________________-')
 
-    console.log(response.data.choices[0].text)
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: messageList,
+  });
 
-    res.status(200).json({text: `${response.data.choices[0].text.trim()}`})
+  console.log(response.data.choices[0].text)
+
+  res.status(200).json({ text: `${response.data.choices[0].text.trim()}` })
 }
