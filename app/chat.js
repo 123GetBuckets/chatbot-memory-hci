bb0014b22dee4ed2fa45e87b3e0641585628f88d
HMISTER <107431892+HMISTER03@users.noreply.github.com>
2023-06-15 01:25:47 -0400

    Update chat.js

--- app/chat.js
+++ app/chat.js
@@ -18,13 +18,15 @@ async function runLLM(messages) {
 export default function Chat() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-
+  const [editMode, setEditMode] = useState(false)
+  const [edit, setEdit] = useState("")
   const [hoveredMessageId, setHoveredMessageId] = useState(null);
+
 
   const handleMouseEnter = (id) => {
     setHoveredMessageId(id);
   };
-  
+
   const handleMouseLeave = () => {
     setHoveredMessageId(null);
   };
@@ -33,6 +35,12 @@ export default function Chat() {
     setMessage(e.target.value);
   };
 
+  const handleEdit = (int, edit) => {
+    const newMessages = [...messages];
+    newMessages[int].content = edit;
+    setMessages(newMessages);
+  }
+
   const handleSend = async () => {
 
     setMessage("");
@@ -40,7 +48,7 @@ export default function Chat() {
     setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "user", content: message }])
 
     const newMessage = { id: uuidv4(), role: "user", content: message };
-    
+
     const messageList = [...messages, newMessage].map(message => ({
       role: message.role,
       content: message.content
@@ -48,7 +56,7 @@ export default function Chat() {
 
     const response = await runLLM(messageList)
 
-    setMessages(prevMessages => [...prevMessages, {id: uuidv4(), role: "assistant", content: response}]);
+    setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "assistant", content: response }]);
 
   };
 
@@ -59,24 +67,43 @@ export default function Chat() {
   return (
     <div className="chat-container">
       <ul className="message-list">
-        {messages.map(msg => 
-            <li key={msg.id} onMouseEnter={() => handleMouseEnter(msg.id)} onMouseLeave={handleMouseLeave}>
-              <div className='message-wrapper'>
-                <div className="message-role">
-                  <span className="role">{msg.role}</span>
-                </div>
-                <div className="message-content">
-                  {msg.content.toLowerCase()}
-                </div>
-                <div className="action-wrapper">
-                  {hoveredMessageId === msg.id && (
-                    <button className="message-actions" onClick={(e) => handleDropdownToggle(e, msg.id)}>
-                      •••
-                    </button>
-                  )}
-                </div>
+        {messages.map((msg, index) =>
+          <li key={msg.id} onMouseEnter={() => handleMouseEnter(msg.id)} onMouseLeave={handleMouseLeave}>
+            <div className='message-wrapper'>
+              <div className="message-role">
+                <span className="role">{msg.role}</span>
               </div>
-            </li>
+              <div className="message-content">
+                {editMode ? (
+                  <div>
+                    <input
+                      type='text'
+                      value={message} onChange={e => setMessage(e.target.value)}
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                          setEditMode(false)
+                          handleEdit(index, message)
+                          setMessage('')
+                        }
+                      }}
+                    />
+                  </div>
+                ) : (
+                  <div onDoubleClick={() => setEditMode(true)}>
+                    {msg.content.toLowerCase()}
+                  </div>
+                )
+                }
+              </div>
+              <div className="action-wrapper">
+                {hoveredMessageId === msg.id && (
+                  <button className="message-actions" onClick={(e) => handleDropdownToggle(e, msg.id)}>
+                    •••
+                  </button>
+                )}
+              </div>
+            </div>
+          </li>
         )}
       </ul>
       <div className="input-container">
