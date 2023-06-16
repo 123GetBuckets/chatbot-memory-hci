6ca04f3364b654c3260b6c7ee9a53671d3fb8b1a
bash <4gutzees@gmail.com>
2023-06-16 13:36:31 -0400

    consolidate icons a bit remove hiddenMessages state add copy button visual feedback add visibility toggle clarify handleEdit by index, not id clear all states on handleNewChat add hard system message

--- app/chat.js
+++ app/chat.js
@@ -1,8 +1,8 @@
 'use client';
 
-import 'styles/chat.css'
-import { GoGrabber, GoKebabHorizontal, GoPerson, GoPlus, GoTriangleRight, GoEye } from "react-icons/go";
-import { TrashIcon, DuplicateIcon, PencilIcon, CopyIcon, EyeIcon, EyeClosedIcon } from '@primer/octicons-react';
+import 'styles/chat.css';
+import { GoKebabHorizontal, GoPlus, GoTriangleRight } from "react-icons/go";
+import { TrashIcon, DuplicateIcon, PencilIcon, CopyIcon, EyeIcon, EyeClosedIcon, CheckIcon } from '@primer/octicons-react';
 import { useState, useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -21,12 +21,12 @@ async function runLLM(messages) {
 export default function Chat() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [hiddenMessages, setHiddenMessages] = useState([]);
   const [editMessageId, setEditMessageId] = useState(null);
   const [edit, setEdit] = useState("")
   const [hoveredMessageId, setHoveredMessageId] = useState(null);
   const [dropdownMessageId, setDropdownMessageId] = useState(null);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [copyClicked, setCopyClicked] = useState(false);
 
   const textAreaRef = useRef(null);
 
@@ -52,8 +52,16 @@ export default function Chat() {
     };
 
     const copyText = () => {
-      navigator.clipboard.writeText(message.content)
-      onClose();
+      navigator.clipboard.writeText(message.content);
+
+      // Set copyClicked state to true when the button is clicked
+      setCopyClicked(true);
+
+      // Change it back to false after 2 seconds
+      setTimeout(() => {
+        setCopyClicked(false);
+      }, 500);
+
     };
 
     const editMessage = () => {
@@ -63,6 +71,14 @@ export default function Chat() {
     };
 
     const editVisibility = () => {
+      const visible = message.visible;
+
+      if (visible) {
+        message.visible = false;
+      } else {
+        message.visible = true;
+      }
+
       onClose();
     };
 
@@ -70,9 +86,13 @@ export default function Chat() {
       <div className="dropdown-menu">
         <button title="Delete" onClick={deleteMessage}><TrashIcon size={16} /></button>
         <button title="Duplicate" onClick={duplicateMessage}><DuplicateIcon size={16} /></button>
-        <button title="Copy" onClick={copyText}><CopyIcon size={16} /></button>
+        <button title="Copy" onClick={copyText}>
+        { copyClicked ? <CheckIcon size={16} /> : <CopyIcon size={16} /> }
+        </button>
         <button title="Edit" onClick={editMessage}><PencilIcon size={16} /></button>
-        <button title="Visibility" onClick={editVisibility}><EyeIcon size={16} /></button>
+        <button title={message.visible ? "Hide" : "Show"} onClick={editVisibility}>
+          {message.visible ? <EyeClosedIcon size={16} /> : <EyeIcon size={16} />}
+        </button>
       </div>
     )
   };
@@ -97,9 +117,9 @@ export default function Chat() {
     e.target.style.height = `${e.target.scrollHeight - 10}px`;
   };
 
-  const handleEdit = (id, edit) => {
+  const handleEdit = (index, edit) => {
     const newMessages = [...messages];
-    newMessages[id].content = edit;
+    newMessages[index].content = edit;
     setMessages(newMessages);
     setEditMessageId(null);
     setEdit('');
@@ -121,43 +141,60 @@ export default function Chat() {
 
   const handleNewChat = () => {
     setMessages([]);
+    setEditMessageId(null);
+    setEdit("")
+    setHoveredMessageId(null);
+    setDropdownMessageId(null);
+    setDropdownOpen(false);
   };
 
   const handleSend = async () => {
 
+    const systemMessage = {
+      role: "system",
+      content: "You are a conversational assistant. Respond as concisely as possible. Use some emojis.",
+    }
+
+    const inputMessage = message;
+    setMessage("");
+
     // Return early if the message is empty
-    if (message.trim() === "") {
+    if (inputMessage.trim() === "") {
 
       if (messages.length === 0) {
         return;
       }
 
-      const messageList = messages.map(message => ({
-        role: message.role,
-        content: message.content
-      }));
+      const messageList = messages
+        .filter((msg => msg.visible))
+        .map(msg => ({
+          role: msg.role,
+          content: msg.content
+        }));
 
-      const response = await runLLM(messageList);
+      const response = await runLLM([systemMessage, ...messageList]);
 
-      setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "assistant", content: response }]);
+      setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "assistant", content: response, visible: true }]);
 
       return;
     }
 
-    setMessage("");
+    setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "user", content: inputMessage, visible: true }])
 
-    setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "user", content: message }])
+    const newMessage = { id: uuidv4(), role: "user", content: inputMessage, visible: true };
 
-    const newMessage = { id: uuidv4(), role: "user", content: message };
+    const newMessages = [...messages, newMessage]
 
-    const messageList = [...messages, newMessage].map(message => ({
-      role: message.role,
-      content: message.content
-    }));
+    const messageList = newMessages
+      .filter(msg => msg.visible)
+      .map(msg => ({
+        role: msg.role,
+        content: msg.content
+      }));
 
-    const response = await runLLM(messageList)
+    const response = await runLLM([systemMessage, ...messageList]);
 
-    setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "assistant", content: response }]);
+    setMessages(prevMessages => [...prevMessages, { id: uuidv4(), role: "assistant", content: response, visible: true }]);
 
   };
 
@@ -169,7 +206,7 @@ export default function Chat() {
       <ul className="message-list">
         {messages.map((msg, index) =>
           <li key={msg.id} onMouseEnter={() => handleMouseEnter(msg.id)} onMouseLeave={handleMouseLeave}>
-            <div className='message-wrapper'>
+            <div className={msg.visible ? 'message-wrapper' : 'message-wrapper message-hidden'}>
               <div className="message-role">
                 <span className="role">{msg.role}</span>
               </div>
